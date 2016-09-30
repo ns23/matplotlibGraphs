729068d420b565ffa045a9a6b47db885cb01ff3a
Nitesh Sawant <niteshsawant023@gmail.com>
2016-09-30 13:25:07 +0000

    hopefully final version

--- bar.py
+++ bar.py
@@ -1,63 +1,56 @@
-#read data from file
-import csv;
-import numpy as np 
+# read data from file
+import csv
+import numpy as np
 import matplotlib.pyplot as plt
 
 data_file = "arabian_sea200602.pix"
 
-
 # rowValue = input("Enter the row value")
-rowValue=4
+rowValue = 4
 
 result = {}
 
 with open(data_file,'r') as f:
-	reader = csv.reader(f,delimiter=' ')
+    reader = csv.reader(f, delimiter=' ')
 
-	for row in reader:
-		if row[0] in result:
-			temp = float(row[rowValue])
-			result[row[0]].append(temp)
-		else:
-			result[row[0]] = [row[rowValue]]
+    for row in reader:
+        if row[0] in result:
+            temp = float(row[rowValue])
+            result[row[0]].append(temp)
+        else:
+            result[row[0]] = [row[rowValue]]
 
-
-#Get all the keys of the list and store it in a tuple
+# Get all the keys of the list and store it in a tuple
 
 keys = list(result.keys())
 keys.sort()
 
-#calculate mean and stanard devaition for the data
+# calculate mean and stanard devaition for the data
 
-resultMean  =  []
-resultStd	=  [] 	
-
+resultMean = []
+resultStd = []
 
 for value in keys:
-	temp = np.array(result[value],dtype="float")
-	resultMean.append(np.mean(temp))
-	resultStd.append(np.std(temp))
-
-
+    temp = np.array(result[value], dtype="float")
+    resultMean.append(np.mean(temp))
+    resultStd.append(np.std(temp))
 
 N = len(resultMean)
-x = range(1,N+1)
-width = 1/1.5
+x = range(1, N + 1)
+width = 1 / 1.5
 
 plt.figure(1)
 plt.title('Bar Graph')
 plt.ylabel('total_AOD fine Mean', fontsize=12)
 plt.xlabel('Date', fontsize=12)
 plt.bar(x, resultMean, width, color="blue")
-plt.xticks(x,size="small")
+plt.xticks(x, size="small")
+
 # plt.show()
 
 
-
-
-
-#draw histogram
+# draw histogram
 plt.figure(2)
-plt.hist(resultMean, bins=N)
+plt.title("Histograph")
+plt.hist(resultMean, bins=N, color="green")
 plt.show()
-
