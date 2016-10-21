2f0af3b072d51aa06f264aeab87df603a0a05767
Nitesh Sawant <niteshsawant023@gmail.com>
2016-10-21 20:31:54 +0530

    code ignores -999 value while reading frrom file

--- freq-histograph.py
+++ freq-histograph.py
@@ -2,13 +2,13 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-
+from collections import Counter
 
 
 data_file = "arabian_sea200602.pix"
 
 #rowValue = input("Enter the row value")
-rowValue = 4
+rowValue = 5
 
 result = []
 
@@ -16,18 +16,22 @@ with open(data_file,'r') as f:
     reader = csv.reader(f, delimiter=' ')
 
     for row in reader:
+        if row[rowValue] == '-999.000000':
+            continue
+
         temp = float(row[rowValue])
         result.append(temp)
 
 
-# sort the result data
+#count of occurance of list items
+occurance_count = Counter(result)
 
+# sort the result data
 sorted_data = sorted(result)
 
 n = len(sorted_data)
-plt.hist(sorted_data,bins=n,color="blue")
+plt.hist(sorted_data,bins=n,color="blue",range=[0,1])
 plt.title("histogram")  #enter ur title here
 plt.xlabel("xlabe")
 plt.ylabel("Frequency")
-
 plt.show()
